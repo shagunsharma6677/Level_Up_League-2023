ea01cdf8ae7b64bd805de13fa1199eb17cb240d2
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-04-07 11:49:14 +0530

    second commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Login from "./pages/login";
-import Register from "./pages/register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
