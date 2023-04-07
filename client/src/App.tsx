89723f33ee45126348b63b09dcac2053bb0adabe
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-04-07 11:40:02 +0530

    second commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Simple React Typescript Tailwind Sample
-      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
