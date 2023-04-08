7d7b256eb873bb895e6e04323c23418a42495b82
uzairansari11 <uzairans532@gmail.com>
2023-04-08 17:56:12 +0530

    login signup functionality fw21_0735

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,16 +1,33 @@
 import { Input } from "@chakra-ui/input";
 import React, { ReactEventHandler, useState } from "react";
-
+import { UserRegister } from "../utils";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
 	const [name, setName] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
+	const [email, setEmail] = useState<string>("");
+	const navigate = useNavigate();
 	const handleRegister = (): void => {
-		const payload = {
+		const payload: UserRegister = {
 			name,
 			password,
+			email,
 		};
 
-		console.log(payload);
+		axios
+			.post(`http://localhost:3001/users/register`, payload)
+			.then((res) => {
+				console.log(res);
+				if (res.status === 200) {
+					navigate("/login", { replace: true });
+				} else if(res.status===201){
+					alert(res.data.message);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 	return (
 		<div>
@@ -25,6 +42,15 @@ const Register = () => {
 			/>
 			<Input
 				type="text"
+				placeholder="Enter Email"
+				size="md"
+				value={email}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+					setEmail(e.target.value)
+				}
+			/>
+			<Input
+				type="text"
 				placeholder="Enter Password"
 				size="md"
 				value={password}
@@ -32,6 +58,7 @@ const Register = () => {
 					setPassword(e.target.value)
 				}
 			/>
+
 			<button onClick={handleRegister}>Register</button>
 		</div>
 	);
